4093c1fcdcbc80fbf3b242ba66ced556e7ddfeae
beco <galashow7@helixtech.co.kr>
2025-12-11 15:11:13 +0900

    Clean repository

--- app/services/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Services package
-"""
-from .weather_service import WeatherService, weather_service
-
-__all__ = ["WeatherService", "weather_service"]
--- app/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Utils package
-"""
-from .api_client import APIClient, get_api_client, close_api_client
-
-__all__ = ["APIClient", "get_api_client", "close_api_client"]
